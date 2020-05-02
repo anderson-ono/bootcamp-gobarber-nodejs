4efeefc6828be86ee9afef0ec1cce220e790abd1
Anderson <andersonono92@hotmail.com>
2020-05-02 04:27:23 -0300

    Bug Tratamento para existencia arquivo

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -22,12 +22,14 @@ class UpdateUserAvatarService {
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
 
-      try {
-        const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-        if (userAvatarFileExists) {
-          await fs.promises.unlink(userAvatarFilePath);
-        }
-      } catch {}
+      const userAvatarFileExists = await fs.promises
+        .stat(userAvatarFilePath)
+        .catch(() => {
+          return false;
+        });
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
     }
 
     user.avatar = avatarFilename;
